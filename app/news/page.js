a8b008eef0da0b227e1c40cd459e1a1803f3e716
Roberto <robertocaravaca436@gmail.com>
2024-06-16 19:06:48 +0200

    New component for display lists

--- app/news/page.js
+++ app/news/page.js
@@ -1,20 +1,12 @@
 import Link from "next/link";
 import { DUMMY_NEWS } from "@/dummy-news";
+import NewsList from "@/components/news-list";
 
 export default function NewsPage() {
     return (
         <div>
             <h1>News Page</h1>
-            <ul className="news-list">
-                {DUMMY_NEWS.map((item) => (
-                    <li key={item.id}>
-                        <Link href={`/news/${item.slug}`}>
-                            <img src={`images/news/${item.image}`} alt={item.title} />
-                            <span>{item.title}</span>
-                        </Link>
-                    </li>
-                ))}
-            </ul>
+           <NewsList news={DUMMY_NEWS}/>
         </div>
     );
 }
